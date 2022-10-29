49030b2cc0d2e2ed117175f34f818031978cde0c
anotherlibrary <junsuk.spot@gmail.com>
2022-10-29 13:07:10 +0900

    initialize library

---
+++ src/main/java/me/else_junsuk/anotherlibrary/AnotherLibrary.java
@@ -0,0 +1,17 @@
+package me.else_junsuk.anotherlibrary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnotherLibrary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
